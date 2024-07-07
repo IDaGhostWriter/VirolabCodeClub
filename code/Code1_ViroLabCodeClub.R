feaab5d6411e90937ad64e27fc6922b3496d8944
IDaGhostWriter <7idaghostwriter@gmail.com>
2024-07-07 18:26:06 +0000

    Lesson1 ViroLabCodeClub

---
+++ code/Code1_ViroLabCodeClub.R
@@ -0,0 +1,15 @@
+
+### IDaGhostWriter
+### ViroLabCodeClub 
+### Session 1
+
+
+##### Check working directory actual
+
+getwd()
+
+##### Change working directory 
+
+setwd("Add the path of the working directory where you want to move")
+
+
